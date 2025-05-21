042b8da0d5ebfb178dd328def23c7b9625732d68
Anton Trochuk <trochuk19@gmail.com>
2025-05-21 22:29:12 +0400

    Верстка блока hero

--- source/js/main.js
+++ source/js/main.js
@@ -7,3 +7,4 @@
 import { wrapperMenu } from './wrapper-menu';
 
 wrapperMenu();
+
--- source/js/wrapper-menu.js
+++ source/js/wrapper-menu.js
@@ -1,10 +1,12 @@
 const buttonContainer = document.querySelector ('.hero__button-container');
+const headerContainer = document.querySelector ('.hero__header-container');
 const buttonToggle = document.querySelector ('.hero__button-toggle');
 
 const wrapperMenu = () => {
   buttonContainer.addEventListener('click', () => {
     buttonContainer.classList.toggle('hero__button-container--active');
     buttonToggle.classList.toggle('hero__button-toggle--active');
+    headerContainer.classList.toggle('hero__header-container--close');
   });
 };
 
