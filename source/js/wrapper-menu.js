00de5010ef1b04fb76a2becf3a58c3c7c65c65ad
Anton Trochuk <trochuk19@gmail.com>
2025-05-24 19:16:28 +0400

    Изменения в hero

--- source/js/wrapper-menu.js
+++ source/js/wrapper-menu.js
@@ -1,12 +1,17 @@
 const buttonContainer = document.querySelector ('.hero__button-container');
 const headerContainer = document.querySelector ('.hero__header-container');
 const buttonToggle = document.querySelector ('.hero__button-toggle');
+const logo = document.querySelector ('.hero__logo');
+const body = document.querySelector('.page-body');
+
 
 const wrapperMenu = () => {
   buttonContainer.addEventListener('click', () => {
     buttonContainer.classList.toggle('hero__button-container--active');
     buttonToggle.classList.toggle('hero__button-toggle--active');
     headerContainer.classList.toggle('hero__header-container--close');
+    logo.classList.toggle('hero__logo--none');
+    body.classList.toggle('body-wrapper-menu');
   });
 };
 
